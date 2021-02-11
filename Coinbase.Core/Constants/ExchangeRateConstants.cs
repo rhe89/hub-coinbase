f27d91743566096da0a705bcac1d18a01b801e84
Roar <roar.h.eriksen@gmail.com>
2021-02-11 20:16:50 +0100

    + Rename

---
+++ Coinbase.Core/Constants/ExchangeRateConstants.cs
@@ -0,0 +1,9 @@
+namespace Coinbase.Core.Constants
+{
+    public static class ExchangeRateConstants
+    {
+        public static readonly string NOK = "NOK";
+        public static readonly string USD = "USD";
+        public static readonly string EUR = "EUR";
+    }
+}
